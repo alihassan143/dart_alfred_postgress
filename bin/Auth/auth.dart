1bed043dc8036b478154e39083b58ccd194d92af
Ali Hassan <alihassan143cool@gmail.com>
2022-02-24 13:02:02 +0500

    comments added for better readibility

--- bin/Auth/auth.dart
+++ bin/Auth/auth.dart
@@ -9,10 +9,15 @@ import '../constants.dart';
 import 'postgress.dart';
 
 class AuthHandler {
+  //api route for login request
   static login(HttpRequest req, HttpResponse res) async {
+    //it accepts the raw json data 
     final body = await req.body as Map<String, dynamic>;
+    //this check that email is exits in the database or not
+
     bool emailExists = await PostGressAuth().checkEmail(body["email"]);
     if (emailExists == true) {
+      //this request the databse the fetch the existing user data if exists it returns the data otherwise exception will return
       final result =
           await PostGressAuth().loginUser(body["email"], body["password"]);
 
@@ -21,9 +26,12 @@ class AuthHandler {
       throw AlfredException(400, {"error": "user  not exists"});
     }
   }
+  //sign up api route
 
   static signup(HttpRequest req, HttpResponse res) async {
+    //it is form data api it accepts all the api data in form 
     final body = await req.bodyAsJsonMap;
+    //check if use exists or not
     bool emailExists = await PostGressAuth().checkEmail(body["email"]);
     if (emailExists == false) {
       // Create the upload directory if it doesn't exist
@@ -38,7 +46,7 @@ class AuthHandler {
       // Create the local file name and save the file
       await File(join('${directory.path}/${uploadedFile.filename}'))
           .writeAsBytes(fileBytes);
-
+//create the user data and return data that is save as json
       final result = await PostGressAuth().signupSave(
           body["email"],
           body["password"],
@@ -51,11 +59,14 @@ class AuthHandler {
       throw AlfredException(400, {"error": "email already exists"});
     }
   }
-
+//
   static sendOtpTOEmail(HttpRequest req, HttpResponse res) async {
+    //it accept the json 
     final body = await req.body as Map<String, dynamic>;
+    //chech user exists or not
     bool emailExists = await PostGressAuth().checkEmail(body["email"]);
     if (emailExists == true) {
+      //return the token and otp to be veirfied
       final result = await PostGressAuth().sendOtpTOEmail(body["email"]);
       return result;
     } else {
@@ -77,6 +88,7 @@ class AuthHandler {
   //   }
   // }
 
+//api route for update the exiting user who forgot the password
   static updatePassword(HttpRequest req, HttpResponse res) async {
     final body = await req.body as Map<String, dynamic>;
     bool emailExists = await PostGressAuth().checkEmail(body["email"]);
